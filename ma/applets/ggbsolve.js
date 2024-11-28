038576ff49b85c0f7d1334667edd6a9f49d31251
cxrvus <benito.m.rabe@gmail.com>
2024-11-28 13:11:51 +0100

    made ggbsolve more concise

--- ma/applets/ggbsolve.js
+++ ma/applets/ggbsolve.js
@@ -1,5 +1,5 @@
-appletCont = document.getElementById('applet_container').firstChild.firstChild;
+appletCont = window.applet_container.firstChild.firstChild;
 ggbBase64 = appletCont.getAttribute('data-param-ggbbase64');
-applet = new GGBApplet({language: 'en_us', ggbBase64});
+applet = new GGBApplet({ggbBase64});
 solution = applet.getAppletObject().getValueString("ListeGewählteAufgabe");
 alert(solution);
