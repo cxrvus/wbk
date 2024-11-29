c5fae812d50e2b2c0f57e0cdde2390458fcff630
cxrvus <benito.m.rabe@gmail.com>
2024-11-29 13:43:04 +0100

    made leakvars standalone

--- ma/applets/godmode.js
+++ ma/applets/godmode.js
@@ -4,7 +4,3 @@ ggbBase64 = appletCont.getAttribute('data-param-ggbbase64');
 applet = new GGBApplet({showMenubar: true, language: 'en_us', ggbBase64});
 applet.inject("applet_container");
 window.appObj = applet.getAppletObject();
-
-godLog = document.createElement('code');
-window.godLog = godLog;
-document.body.appendChild(godLog);
--- ma/applets/leakvars.js
+++ ma/applets/leakvars.js
@@ -1,10 +1,19 @@
-appObj = window.appObj;
-keys = appObj.getAllObjectNames();
+appletCont = window.applet_container.firstChild.firstChild;
+ggbBase64 = appletCont.getAttribute('data-param-ggbbase64');
+applet = new GGBApplet({ggbBase64});
+obj = applet.getAppletObject();
+
+keys = obj.getAllObjectNames();
 values = keys.map((key, i) => ({
 	index: i,
 	name: key,
-	value: appObj.getValueString(key),
-	def: appObj.getDefinitionString(key)
+	value: obj.getValueString(key),
+	def: obj.getDefinitionString(key)
 }));
 
-window.godLog.innerText = JSON.stringify(values, null, 4);
+if (!window.godLog) {
+	godLog = document.createElement('code');
+	window.godLog = godLog;
+	document.body.appendChild(godLog);
+}
+else window.godLog.innerText = JSON.stringify(values, null, 4);
