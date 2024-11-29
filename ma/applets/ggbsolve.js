d7efef2c11eccf0c342c6d4dfda0e662f8390fb4
cxrvus <benito.m.rabe@gmail.com>
2024-11-29 13:31:51 +0100

    added more possible solution var keys

--- ma/applets/ggbsolve.js
+++ ma/applets/ggbsolve.js
@@ -1,5 +1,8 @@
 appletCont = window.applet_container.firstChild.firstChild;
 ggbBase64 = appletCont.getAttribute('data-param-ggbbase64');
 applet = new GGBApplet({ggbBase64});
-solution = applet.getAppletObject().getValueString("ListeGewählteAufgabe");
+keys = ['strAuswertung1', 'ListeGewählteAufgabe', 'ListeLösungen'];
+obj = applet.getAppletObject();
+key = keys.find(x => obj.getValueString(x));
+solution = key ? obj.getValueString(key) : 'could not find solution';
 alert(solution);
