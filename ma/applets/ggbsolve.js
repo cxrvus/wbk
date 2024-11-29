76bd2b8019c839a62be013761a144b66873589a4
cxrvus <benito.m.rabe@gmail.com>
2024-11-29 13:40:42 +0100

    misc fmt

--- ma/applets/ggbsolve.js
+++ ma/applets/ggbsolve.js
@@ -1,8 +1,9 @@
 appletCont = window.applet_container.firstChild.firstChild;
 ggbBase64 = appletCont.getAttribute('data-param-ggbbase64');
 applet = new GGBApplet({ggbBase64});
-keys = ['strAuswertung1', 'ListeGewählteAufgabe', 'ListeLösungen'];
 obj = applet.getAppletObject();
+
+keys = ['strAuswertung1', 'ListeGewählteAufgabe', 'ListeLösungen'];
 key = keys.find(x => obj.getValueString(x));
 solution = key ? obj.getValueString(key) : 'could not find solution';
 alert(solution);
