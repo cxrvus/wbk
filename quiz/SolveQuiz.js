a0ca471c1e515a69feb9bf876a0e495c0884dd8d
cxrvus <benito.m.rabe@gmail.com>
2025-01-27 10:14:54 +0100

    updated script to also work for card-ordering tasks

--- quiz/SolveQuiz.js
+++ quiz/SolveQuiz.js
@@ -1,1 +1,3 @@
-alert(JSON.stringify(Object.fromEntries(window[0].questions.map(q=>q.answers.find(a=>a.check.value)?.answer.value).map((a,i)=>[i,a])),null,2))
+answerData = window[0].questions?.map(q=>q.answers.find(a=>a.check.value)?.answer.value)?.map((a,i)=>[i,a])
+?? window[0].cardList.map((x,i) => [i, x.xmlValue]);
+alert(JSON.stringify(Object.fromEntries(answerData),null,2))
